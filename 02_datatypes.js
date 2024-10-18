b6fc44fc0a236c5019dfebf7a4048cca17c2a277
ayushi2112arora <ayushi18arora@gmail.com>
2024-10-18 15:41:57 +0530

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js as a newer version
+// alert(3+4) we are using node js ,not browser
+
+// code readability is high
+
+let name = 'ps'
+let age = 54
+let islogicIn = false 
+
+//number =>2 to power 54
+// bigInt
+// string => ""
+// boolean => t/f
+// null=> standalone value
+// undefined=>koi value nhi de rakhi hai
+// symbol => unique
+// object
+console.log(typeof undefined); //undefined aayega
+console.log(typeof null); //object
+
