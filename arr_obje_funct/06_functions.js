59fe5e75413de1566a69b5cb3b3dd2c27b3fccf4
ayushi2112arora <ayushi18arora@gmail.com>
2024-11-13 20:03:30 +0530

    CHANGES

--- arr_obje_funct/06_functions.js
+++ arr_obje_funct/06_functions.js
@@ -1,63 +1,81 @@
-function myname(){
-console.log('a');
-console.log('y');
-console.log('u');
-console.log('s');
-console.log('h');
-console.log('i');
+function myname() {
+    console.log('a');
+    console.log('y');
+    console.log('u');
+    console.log('s');
+    console.log('h');
+    console.log('i');
 }
 //myname() //parenthesis means execution of the function and if we not use parenthesis then we call reference
 
 // function sumtwonum(num1,num2) {
 //     console.log(num1 + num2);
-    
+
 // }
-function sumtwonum(num1,num2) {
+function sumtwonum(num1, num2) {
     // let result = num1 + num2
     // //console.log('hdsduh');
     // return result
     // //console.log('hdsduh');//return ke baad mein likhtai hai toh run nhi hoga output nhi aayega
     return num1 + num2
-    
+
 }
 //sumtwonum(4,5) //values dete hai jhabh uskoh apn argument boltai hai
 //sumtwonum(4,'6')
-const result = sumtwonum(4,6)
+const result = sumtwonum(4, 6)
 //numbers lete hai toh usko parameters lete hai
-console.log('Result: ',result); // return apne aap mein ek concept hai
+console.log('Result: ', result); // return apne aap mein ek concept hai
 
-function loginuser(username ='samer') {
-    if(!username){
+function loginuser(username = 'samer') {
+    if (!username) {
         console.log('please enter a username');
         return
     }
 
-    return`${username} just logged in`
+    return `${username} just logged in`
 }
 console.log(loginuser())
 
-function username(val1,val2,...name) { //rest operator bundle mein pack kro aur uskoh de do multi values
+function username(val1, val2, ...name) { //rest operator bundle mein pack kro aur uskoh de do multi values
     return name
 }
-console.log(username('prince','ayushi','rashi'));//array ki format deta hai
+console.log(username('prince', 'ayushi', 'rashi'));//array ki format deta hai
 
 const user = {
-    username:'ayushi',
-    price:1555
+    username: 'ayushi',
+    price: 1555
 }
 function handleobject(anyobject) {
-    console.log(`username is ${anyobject.username } and price is ${anyobject.price}`);
-    
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+
 }
 handleobject(user)
 handleobject({
-    username:'yogi',
-    price:599,
+    username: 'yogi',
+    price: 599,
 })
 
-const mynewarray=[200,400,600,800]
+const mynewarray = [200, 400, 600, 800]
 function returnsecondvalue(mynewarray) {
     return mynewarray[1]
 }
 //console.log(returnsecondvalue(mynewarray));
-console.log(returnsecondvalue([200,400,600,800]));
+console.log(returnsecondvalue([200, 400, 600, 800]));
+
+function multiplytwonum(num1, num2) {
+    return num1 * num2
+}
+console.log(multiplytwonum(8, 6))
+
+const userlogged = {
+    username: 'prince-gehlot',
+    id: 2,
+    graduation: 'mca2024-2025'
+}
+function logged(log) {
+    console.log(`username is ${log.username} the id of ${log.id} and the graduation completed in the year ${log.graduation}`);
+
+}
+logged(userlogged)
+
+
